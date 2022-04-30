7efc8cce8a3ff6d4ad12b089d8edf6ec361c6989
junhyuk <jnhk123@naver.com>
2022-04-30 13:55:37 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,14 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach(function (to, from, next) {
+    next();
+});
+
+router.afterEach(function (to, from) {
+    
+});
 
 export default router
