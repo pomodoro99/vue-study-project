2ec6ef8d8aad2e48033ad94c34b6679d6c198759
junhyuk <jnhk123@naver.com>
2022-05-01 10:17:13 +0900

    수정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,39 @@
 <template>
+
 </template>
 
 <script>
 
 export default {
   name: 'HomeView',
+
+  created() {
+
+    setTimeout(()=> {
+      console.log(1);
+    }, this.getRandomMilis());
+
+    setTimeout(()=> {
+      console.log(2);
+    }, this.getRandomMilis());
+
+    setTimeout(()=> {
+      console.log(3);
+    }, this.getRandomMilis());
+
+    setTimeout(()=> {
+      console.log(4);
+    }, this.getRandomMilis());
+
+  },
+
+  methods: {
+    getRandomMilis(){
+      return Math.ceil(Math.random() * 10000);
+    }
+  },
+
   
+
 }
 </script>
